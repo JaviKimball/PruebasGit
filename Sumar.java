c045e27e08e6fa6bc5bb74ba0118667f8b36e6ac
Javier Cencerrado Ruiz <Javier Cencerrado Ruiz>
2016-10-26 10:42:30 +0200

    Creando clase suma

---
+++ Sumar.java
@@ -0,0 +1,16 @@
+
+public class Sumar {
+
+	public static void main (String[]args){
+		
+		
+		int a = 5;
+		int b = 6;
+		int suma = a+b;
+		
+		System.out.println("La suma de "+a+" y "+b+" es: "+suma);
+		
+	}
+	
+	
+}
