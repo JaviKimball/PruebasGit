e8d6529472c6c6e8527269930cf7a215cffc128a
Kimball <a@b.es>
2016-10-26 10:51:57 +0200

    Añadida la resta

--- Sumar.java
+++ Sumar.java
@@ -7,8 +7,11 @@ public class Sumar {
 		int a = 5;
 		int b = 6;
 		int suma = a+b;
+		int resta = a-b;
 		
 		System.out.println("La suma de "+a+" y "+b+" es: "+suma);
+		
+		System.out.println("La resta de "+a+" y "+b+" es: "+resta);
 		
 	}
 	
